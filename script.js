993ef0c8a3fa88f733fa169c7ed3bdefdeb91f8e
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2024-09-16 21:10:12 -0300

    up-2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function mudarModo(modo) {
     const body = document.body;
   
     // Remover classes de outros modos
-    body.classList.remove('modo-claro', 'modo-escuro', 'modo-kawaii', 'modo-azulClaro', 'modo-verdeClaro');
+    body.classList.remove('modo-claro', 'modo-escuro', 'modo-rosa', 'modo-azulClaro', 'modo-verdeClaro');
   
     // Adicionar a classe do modo escolhido
     body.classList.add(`modo-${modo}`);
